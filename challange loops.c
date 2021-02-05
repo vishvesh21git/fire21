64f5f50601d0db42a191bb6ac846d9b0db32d860
vishvesh21git <78500165+vishvesh21git@users.noreply.github.com>
2021-02-05 18:34:03 +0530

    Create challange loops.c
    challage loops

---
+++ challange loops.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdib.h>
+int main ()
+{
+  int i,j,rows;
+    printf("enter the number of rows");
+    scanf("%d",&rows);
+    for(i=1;i<=rows;i++)
+    {
+    for(J=1;J<=rows;j++){
+    if(j%2==1)
+    {
+    printf("0");
+    }
+    else{
+    printf(1);
+    }
+}
+getch();
+return 0;
+}
